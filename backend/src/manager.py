2b9d08d7313ca6e8df13923499aacc4134c23b32
S H 3 Z <185839606+sh3zTheAlien@users.noreply.github.com>
2025-08-20 04:11:27 +0200

    Update manager.py

--- backend/src/manager.py
+++ backend/src/manager.py
@@ -3,17 +3,3 @@ from spotify import SpotifyManager
 
 ytmusic = YTManager()
 spotify = SpotifyManager()
-
-#print(ytmusic.get_playlists())
-searched_playlist = ytmusic.search_playlist('LM') # we need the playlist id to be returned when a playlist gets pressed
-#for track in searched_playlist["tracks"]:
-#print(spotify.search_song(searched_playlist["tracks"][0]["title"]))
-print(ytmusic.search_songs(["Interstellar"]))
-
-#when selected playlist for transfer
-#get the playlists songs,ids info # Done with YTMusic
-#search given songs in ytmusic # Do it in Spotify
-#validate and then
-#Validation:
-#if song duration,aritst name,title is the same then pass else cut
-#create playlist or add to playlist
