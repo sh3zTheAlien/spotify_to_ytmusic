ab749d49aa2d36f7c054277640b6a15b055c79fb
sh3zTheAlien <sh3zthealien@protonmail.com>
2025-08-21 04:00:07 +0300

    manager stage 1

--- src/manager.py
+++ src/manager.py
@@ -5,12 +5,22 @@ from rapidfuzz import fuzz
 def validate_song(query_song,target_song):
     """Validates the Similarity between 2 strings in range of 0 - 100."""
     name_score = fuzz.token_set_ratio(query_song, target_song)
-    print(f"Searched:{query_song} | Validated: {target_song} | Similarity: {name_score}")
+    print(f"Searched:{query_song} | Validated: {target_song} | Similarity: {name_score}\n")
     return name_score
 
 ytmusic = YTManager()
 spotify = SpotifyManager()
 
-searched_ytplaylist = ytmusic.search_playlist('LM')
-searched_song = spotify.search_song(song_title=searched_ytplaylist[0]["title"])
-print(f"YTPLAYLIST: \n{searched_ytplaylist}\n\n SONG: \n{searched_song}")
+ytplaylist_songs = ytmusic.search_playlist('LM')
+
+for ytsong in range(len(ytplaylist_songs)):
+    target_song = ytplaylist_songs[ytsong]["title"]
+    searched_song = spotify.search_song(target_song)
+    print("***************************************************************")
+    for spsong in range(len(searched_song)):
+        query_song = searched_song[spsong]["title"]
+        validate_song(query_song,target_song)
+        #add some exceptions in case there are no results
+
+
+
