171f60f6d6f62000c066cf4ce96f83e16b0b2df7
sh3zTheAlien <sh3zthealien@protonmail.com>
2025-08-25 01:23:01 +0300

    backend_flask-cors_enabled

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,15 +1,20 @@
 from flask import Flask
 from ytmusic import YTManager
 import json
+from flask_cors import CORS, cross_origin
 
 ytmusic = YTManager()
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @app.route("/")
+@cross_origin()
 def get_playlists():
     playlists = ytmusic.get_playlists()
-    return playlists
+    return json.dumps(playlists)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
